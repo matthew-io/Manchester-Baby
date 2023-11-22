8f0054484bef2b19d4ef37fdc8c89fde42c23a5d
Drew Webster <d.webster4034@gmail.com>
2023-11-22 11:48:18 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 using namespace std;
-#include "baby.h"
+#include "Baby.h"
 #include <iostream>
 #include <string>
 
 int main()
 {
-    string binary = "0001";
-    int decimal = binaryToDecimal(binary);
-    cout << decimal;
-    Baby *baby = new Baby();
-    baby->printStore();
+
+
+    //string binary = "110";
+    //int decimal = baby->binaryToDecimal(binary);
+    //cout << decimal << endl;
+
+
+    Baby* baby = new Baby(); // Assuming you've properly allocated memory for the Baby object
+
+    // Initial value of currentCmd
+    cout << "Initial Address: " << baby->getCurrentCmdAddress() << endl;
+
+    // Increment and print multiple times
+    for (int i = 0; i < 3; ++i) {
+        baby->incrementCurrentCmd();
+        cout << "After Increment " << i + 1 << ": " << baby->getCurrentCmdAddress() << endl;
+    }
+
+    delete baby; // Don't forget to free the allocated memory
+
 
     // delete baby;
     return 0;
